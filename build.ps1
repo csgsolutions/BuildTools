bcf3910b7e9d29347e77e743b2f887f905426fb1
Justin Buchanan <buchanan@csgsolutions.com>
2018-01-09 09:34:40 -0600

    Add wildcard versions

--- build.ps1
+++ build.ps1
@@ -19,11 +19,9 @@ $BuildNumber = $BuildNumber.PadLeft(5, "0")
 # duplicate logic in version.props for zip and nuspec
 $PackageVersion = "$VersionPrefix-$VersionSuffix-$BuildNumber"
 $ZipVersion = "$VersionPrefix-$VersionSuffix"
-if ($IsFinalBuild -eq "true" -and $VersionSuffix -eq "rtm"){
+if ($VersionSuffix -eq "stable"){
     $PackageVersion = "$VersionPrefix"
     $ZipVersion = "$VersionPrefix"
-} elseif ($IsFinalBuild -eq "true") {
-    $PackageVersion = "$VersionPrefix-$VersionSuffix-final"
 }
 
 Write-Host "Cleanup" -ForegroundColor Magenta
@@ -33,7 +31,7 @@ Write-Host "Building Csg.Build.Metadata..." -ForegroundColor Magenta
 Push-Location ./src/Csg.Build.Metadata
 & dotnet restore
 #& dotnet build --configuration Release /p:BuildNumber=$BuildNumber /p:ProjVersionSuffix=$VersionSuffix /p:ProjVersionPrefix=$VersionPrefix /p:IsFinalBuild=$IsFinalBuild
-& dotnet pack --configuration Release --output ../../bin /p:BuildNumber=$BuildNumber /p:RepoVersionSuffix=$VersionSuffix /p:RepoVersionPrefix=$VersionPrefix /p:IsFinalBuild=$IsFinalBuild
+& dotnet pack --configuration Release --output ../../bin /p:BuildNumber=$BuildNumber /p:RepoVersionSuffix=$VersionSuffix /p:RepoVersionPrefix=$VersionPrefix
 Pop-Location
 
 Write-Host "Building Csg.Build.Metadata.Tasks NuGet package..." -ForegroundColor Magenta
@@ -43,6 +41,18 @@ Pop-Location
 
 Write-Host "Creating BuildTools ZIP" -ForegroundColor Magenta
 $PackageVersion | Out-File .\src\Tools\VersionInfo.txt
-Compress-Archive -Force -Path .\src\Tools\* -DestinationPath ".\bin\BuildTools-$ZipVersion.zip"
+
+$ZipPath = ".\bin\BuildTools-$ZipVersion.zip"
+Compress-Archive -Force -Path .\src\Tools\* -DestinationPath $ZipPath
+
+$VersionParts = $VersionPrefix.Split(".")
+$LatestMajorVersion = "$($VersionParts[0])-$VersionSuffix"
+$LatestMinorVersion = "$($VersionParts[0]).$($VersionParts[1])-$VersionSuffix"
+$LatestBuildVersion = "$VersionPrefix-$VersionSuffix"
+Copy-Item -Path $ZipPath -Destination ".\bin\BuildTools-$LatestMajorVersion.zip"
+Copy-Item -Path $ZipPath -Destination ".\bin\BuildTools-$LatestMinorVersion.zip"
+if ($VersionSuffix -eq "stable"){
+    Copy-Item -Path $ZipPath -Destination ".\bin\BuildTools-$LatestBuildVersion.zip"
+}
 
 Write-Host "All good!" -ForegroundColor Green
